85bad3d6a655c2ac8a49caca746cc4b73855fb1e
skkzsh <skkzsh@myopera.com>
2012-09-26 22:15:08 +0900

    bin

--- bin/gitmodule.sh
+++ bin/gitmodule.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-git submodule add https://github.com/wanderlust/apel.git public_repos/apel
-git submodule add https://github.com/wanderlust/semi.git public_repos/semi
-git submodule add https://github.com/wanderlust/flim.git public_repos/flim
+git submodule add https://github.com/wanderlust/apel.git                         public_repos/apel
+git submodule add https://github.com/wanderlust/semi.git                         public_repos/semi
+git submodule add https://github.com/wanderlust/flim.git                         public_repos/flim
 git submodule add https://github.com/tequilasunset/auto-complete-latex-light.git public_repos/auto-complete-latex-light
-git submodule add https://github.com/myuhe/calfw-gcal.el.git public_repos/calfw-gcal.el
-git submodule add https://github.com/knu/elscreen.git public_repos/elscreen
-git submodule add https://github.com/kiwanami/emacs-calfw.git public_repos/emacs-calfw
-git submodule add https://github.com/magit/magit.git public_repos/magit
-git submodule add https://github.com/capitaomorte/yasnippet.git public_repos/yasnippet
-git submodule add http://jblevins.org/git/markdown-mode.git public_repos/markdown-mode
-git submodule add https://github.com/jwiegley/org-mode.git public_repos/org-mode
+git submodule add https://github.com/myuhe/calfw-gcal.el.git                     public_repos/calfw-gcal.el
+git submodule add https://github.com/knu/elscreen.git                            public_repos/elscreen
+git submodule add https://github.com/kiwanami/emacs-calfw.git                    public_repos/emacs-calfw
+git submodule add https://github.com/magit/magit.git                             public_repos/magit
+git submodule add https://github.com/capitaomorte/yasnippet.git                  public_repos/yasnippet
+git submodule add http://jblevins.org/git/markdown-mode.git                      public_repos/markdown-mode
+git submodule add https://github.com/jwiegley/org-mode.git                       public_repos/org-mode
+git submodule add https://github.com/eschulte/org-S5.git                         public_repos/org-s5
+git submodule add https://github.com/maple/emacsfiles.git                        public_repos/emacsfiles
