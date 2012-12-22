74085f8f5f02a38af85d51db44c9448149c43876
skkzsh <skkzsh@myopera.com>
2012-12-22 22:27:31 +0900

    edit

--- conf/edit-lisp-init.el
+++ conf/edit-lisp-init.el
@@ -20,10 +20,19 @@
 ; (add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete-1.4/dict")
 ; (ac-config-default)
 
+;; Sources
+;; (push 'ac-source-filename ac-sources)
+;; or
+;; (setq ac-sources '(
+;;                    ac-source-filename
+;;                    ac-source-words-in-buffer
+;;                    ac-source-words-in-same-mode-buffers
+;;                    ))
+
 ; (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
-; (global-set-key "\M-/" 'ac-start)
+(global-set-key "\M-/" 'ac-start)
 ;; 補完ウィンドウでC-p,C-nで候補選択
-; (setq ac-use-menu-map t)
+(setq ac-use-menu-map t)
 
 ;; カレントバッファ以外の補完機能は２文字目から作動させる。
 ;(setq ac-dabbrev-all-min-count 2)
@@ -33,7 +42,7 @@
       (append ac-modes
               (list
                'yatex-mode
-               ;; 'org-mode
+               'org-mode
                )))
 
 ;; Lisp
@@ -46,3 +55,6 @@
 ;;;; Junk File
 (require 'open-junk-file)
 (setq open-junk-file-format "~/.junk/%Y-%m%d-%H%M.")
+
+;; for Dropbox
+;; (setq open-junk-file-format "~/Dropbox/.junk/%Y-%m%d-%H%M.")
