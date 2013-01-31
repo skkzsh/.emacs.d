5a83d1b9aa234b035521a5a9b0b0dd8937978bdf
skkzsh <skkzsh@myopera.com>
2013-02-01 02:49:57 +0900

    auto-install elpa

--- elpa/ace-jump-mode-2.0.0.0/ace-jump-mode-autoloads.el
+++ elpa/ace-jump-mode-2.0.0.0/ace-jump-mode-autoloads.el
@@ -3,9 +3,34 @@
 ;;; Code:
 
 
-;;;### (autoloads (ace-jump-mode) "ace-jump-mode" "ace-jump-mode.el"
-;;;;;;  (20434 21193))
+;;;### (autoloads (ace-jump-mode ace-jump-line-mode ace-jump-word-mode
+;;;;;;  ace-jump-char-mode ace-jump-mode-pop-mark) "ace-jump-mode"
+;;;;;;  "ace-jump-mode.el" (20710 53625))
 ;;; Generated autoloads from ace-jump-mode.el
+
+(autoload 'ace-jump-mode-pop-mark "ace-jump-mode" "\
+Pop up a postion from `ace-jump-mode-mark-ring', and jump back to that position
+
+\(fn)" t nil)
+
+(autoload 'ace-jump-char-mode "ace-jump-mode" "\
+AceJump char mode
+
+\(fn QUERY-CHAR)" t nil)
+
+(autoload 'ace-jump-word-mode "ace-jump-mode" "\
+AceJump word mode.
+You can set `ace-jump-word-mode-use-query-char' to nil to prevent
+asking for a head char, that will mark all the word in current
+buffer.
+
+\(fn HEAD-CHAR)" t nil)
+
+(autoload 'ace-jump-line-mode "ace-jump-mode" "\
+AceJump line mode.
+Marked each no empty line and move there
+
+\(fn)" t nil)
 
 (autoload 'ace-jump-mode "ace-jump-mode" "\
 AceJump mode is a minor mode for you to quick jump to a
@@ -25,13 +50,13 @@ If you don't like the default move keys, you can change it by
 setting `ace-jump-mode-move-keys'.
 
 You can constrol whether use the case sensitive via
-`ace-jump-mode-case-sensitive-search'.
+`ace-jump-mode-case-fold'.
 
 \(fn &optional PREFIX)" t nil)
 
 ;;;***
 
-;;;### (autoloads nil nil ("ace-jump-mode-pkg.el") (20434 21193 629089))
+;;;### (autoloads nil nil ("ace-jump-mode-pkg.el") (20710 53625 651631))
 
 ;;;***
 
