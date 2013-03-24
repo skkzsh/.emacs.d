ea1e81875db7825ea2ea2c8547909d9fb42d12cb
skkzsh <skkzsh@myopera.com>
2013-03-25 01:24:09 +0900

    Load csv-mode

--- conf/develop-init.el
+++ conf/develop-init.el
@@ -122,3 +122,11 @@
 ;;;; blockdiag
 ;; TODO
 (load "~/.emacs.d/public_repos/emacsfiles/blockdiag-mode.el")
+
+;---------------------------------------------------------------------------
+;;;; CSV
+;; TODO
+(add-to-list 'auto-mode-alist
+             '("\\.[Cc][Ss][Vv]\\'" . csv-mode))
+(autoload 'csv-mode "csv-mode"
+  "Major mode for editing comma-separated value files." t)
