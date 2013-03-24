b16a2067c20b28f0f2864f85cf76b3faed8d5753
skkzsh <skkzsh@myopera.com>
2013-03-25 01:41:26 +0900

    Load aspell

--- conf/lisp-init.el
+++ conf/lisp-init.el
@@ -1,16 +1,26 @@
+;;;; Basic Setttings by Extensions
+;---------------------------------------------------------------------------
 ;;;; sequential-command
 ;; (require 'sequential-command-config)
 ;; (sequential-command-setup-keys)
 
-
+;---------------------------------------------------------------------------
 ;;;; Key Chord
 ;; (key-chord-mode t)
 ;; (setq key-chord-two-keys-delay 0.05)
 
+;---------------------------------------------------------------------------
+;;;; Spell
+(setq ispell-program-name "aspell")
+; (setq ispell-grep-command "grep")
+; (eval-after-load "ispell"
+;                  '(add-to-list 'ispell-skip-region-alist '("[^\000-\377]")))
+;; flyspell
 
+;---------------------------------------------------------------------------
 ;;;; Vi
 
-
+;---------------------------------------------------------------------------
 ;;;; APEL
 (setq load-path
       (append '(
