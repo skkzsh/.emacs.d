4dc884db20e47012b6cb034762d179ecaaf46f62
skkzsh <skkzsh12@gmail.com>
2014-06-20 18:26:24 +0900

    Modify anything-init

--- conf/anything-init.el
+++ conf/anything-init.el
@@ -30,20 +30,23 @@
 
   ;;;; c-moccur
   ;; TODO
-  (add-to-list 'load-path "~/.emacs.d/public_repos/anything-c-moccur")
-  (require 'anything-c-moccur)
+  (when (require 'color-moccur nil t)
 
-  ;;; カスタマイズ可能変数の設定(M-x customize-group anything-c-moccur でも設定可能)
-  (setq anything-c-moccur-anything-idle-delay 0.2 ; `anything-idle-delay'
-        anything-c-moccur-higligt-info-line-flag t ; `anything-c-moccur-dmoccur'などのコマンドでバッファの情報をハイライトする
-        anything-c-moccur-enable-auto-look-flag t ; 現在選択中の候補の位置を他のwindowに表示する
-        anything-c-moccur-enable-initial-pattern t) ; `anything-c-moccur-occur-by-moccur'の起動時にポイントの位置の単語を初期パターンにする
+    (add-to-list 'load-path "~/.emacs.d/bundle/anything-c-moccur/trunk")
+    (require 'anything-c-moccur)
 
-  (global-set-key (kbd "M-o") 'anything-c-moccur-occur-by-moccur) ; バッファ内
-  (global-set-key (kbd "C-M-o") 'anything-c-moccur-dmoccur) ; ディレクトリ
-  (add-hook 'dired-mode-hook ; dired
-            '(lambda ()
-               (local-set-key (kbd "O") 'anything-c-moccur-dired-do-moccur-by-moccur)))
+    ;;; カスタマイズ可能変数の設定(M-x customize-group anything-c-moccur でも設定可能)
+    (setq anything-c-moccur-anything-idle-delay 0.2 ; `anything-idle-delay'
+          anything-c-moccur-higligt-info-line-flag t ; `anything-c-moccur-dmoccur'などのコマンドでバッファの情報をハイライトする
+          anything-c-moccur-enable-auto-look-flag t ; 現在選択中の候補の位置を他のwindowに表示する
+          anything-c-moccur-enable-initial-pattern t) ; `anything-c-moccur-occur-by-moccur'の起動時にポイントの位置の単語を初期パターンにする
+
+    (global-set-key (kbd "M-o") 'anything-c-moccur-occur-by-moccur) ; バッファ内
+    (global-set-key (kbd "C-M-o") 'anything-c-moccur-dmoccur) ; ディレクトリ
+    (add-hook 'dired-mode-hook ; dired
+              '(lambda ()
+                 (local-set-key (kbd "O") 'anything-c-moccur-dired-do-moccur-by-moccur)))
+    )
 
   ;;;; Ack
   ;; (when (executable-find "ack")
