d3595ca6d43da6e07f486054f40f3d65f3b63d6c
skkzsh <skkzsh12@gmail.com>
2019-07-06 19:56:59 +0900

    Update .emacs

--- conf/develop-init.el
+++ conf/develop-init.el
@@ -1,9 +1,9 @@
 ;;;; Development Settings
-;---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
 ;;;; Magit
 ;; (require 'magit)
 
-;---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
 ;;;; Git gutter
 
 ;; (add-to-list 'load-path "~/.emacs.d/bundle/emacs-git-gutter")
@@ -20,7 +20,7 @@
 ;; (add-to-list 'load-path "~/.emacs.d/bundle/emacs-git-gutter-fringe")
 ;; (require 'git-gutter-fringe)
 
-;---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
 ;;;; HTML
 
 ;;;; See markdown-open
@@ -41,17 +41,26 @@
                     nil nil nil buffer-file-name))))
 
 ;; (add-hook 'html-mode-hook
-;;            (lambda()
+;;            (lambda ()
 ;;              (define-key html-mode-map (kbd "C-c C-c o") 'html-open)
 ;;              )
 ;;            )
 
-;---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
+;;;; Web
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (setq js2-basic-offset 2)
+            (setq js-switch-indent-offset 2)
+            ))
+
+;;---------------------------------------------------------------------------
 ;;;; YAML
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
-;---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
 ;;;; reStructuredText
 ;; TODO
 (setq frame-background-mode 'dark) ; 背景に合わせて (requireの前)
@@ -61,25 +70,25 @@
 
 ;; (add-hook 'rst-adjust-hook 'rst-toc-update)
 (add-hook 'rst-mode-hook
-              (lambda ()
-                (cond
-                 ;;; Linux
-                 ;; ((eq system-type 'gnu/linux)
-                 ;;  (setq rst-pdf-program "apvlv")
-                 ;;  (setq rst-slides-program "opera")
-                 ;;  )
-                 ;;; Mac
-                 ((eq system-type 'darwin)
-                  (setq rst-pdf-program "open"
-                        rst-slides-program "open -a Safari")
-                  )
-                 ;;; Windows
-                 ;; ((eq system-type 'windows-nt)
-                 ;;  (setq rst-pdf-program "")
-                 ;;  (setq rst-slides-program "")
-                 ;;  )
-                ))
+          (lambda ()
+            (case system-type
+             ;;; Linux
+             ;; (gnu/linux
+             ;;  (setq rst-pdf-program "apvlv")
+             ;;  (setq rst-slides-program "opera")
+             ;;  )
+             ;;; Mac
+             (darwin
+              (setq rst-pdf-program "open"
+                    rst-slides-program "open -a Safari")
               )
+             ;;; Windows
+             ;; (windows-nt
+             ;;  (setq rst-pdf-program "")
+             ;;  (setq rst-slides-program "")
+             ;;  )
+             ))
+          )
 
 ;; HTMLが存在すれば, 保存後にExport
 ;; TODO
@@ -88,12 +97,12 @@
 ;; TOOD: make html & view のKeybind
 ;; browse-url
 
-;---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
 ;;;; blockdiag
 ;; TODO
 ;; (load "~/.emacs.d/el-get/emacsfiles/blockdiag-mode.el")
 
-;---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
 ;;;; CSV
 ;; TODO
 (add-to-list 'auto-mode-alist '("\\.csv$" . csv-mode))
