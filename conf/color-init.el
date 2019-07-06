2fbc6cd92785966fbf52b1de2466471d165944dd
skkzsh <skkzsh12@gmail.com>
2019-07-06 19:56:38 +0900

    Update color config

--- conf/color-init.el
+++ conf/color-init.el
@@ -1,48 +1,26 @@
 ;;;; Color Settings
 
-;---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
 ;;;;; Color Theme
-(add-to-list 'load-path "~/.emacs.d/lisp/color-theme")
-(add-to-list 'load-path "~/.emacs.d/bundle/emacs-color-theme-solarized")
-(require 'color-theme)
-(color-theme-initialize)
-;; TODO
+;; (setq custom-theme-directory "~/.emacs.d/themes/")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/el-get/zenburn-theme")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/el-get/zenburn-emacs")
 
-(cond
- ((eq window-system 'x)
-  (color-theme-hrehf-dark)
+(case window-system
+  ((x)      (load-theme 'wombat t))
+  ((mac ns) (load-theme 'zenburn t))
+  ((w32)    (load-theme 'wombat t))
   )
- ((eq window-system 'ns)
-  (color-theme-wombat)
-  )
- ((eq window-system 'w32)
-  (color-theme-wombat)
-  )
- )
 
-;; (when window-system
-;;   (color-theme-hrehf-dark)
-;;   ;; (color-theme-wombat)
-;;   ;; (color-theme-railscasts)
-;;   )
-
-;; (when (>= emacs-major-version 24)
-;; (load-theme 'molokai t)
-;; (enable-theme 'molokai)
-;; )
-
-;---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
 ;;; 透明度
-(cond
- ((eq window-system 'x)
-  (add-to-list 'default-frame-alist '(alpha . 90)))
- ((eq window-system 'ns)
-  (add-to-list 'default-frame-alist '(alpha . 90)))
- ((eq window-system 'w32)
-  (add-to-list 'default-frame-alist '(alpha . 90)))
+(case window-system
+  ((x)      (add-to-list 'default-frame-alist '(alpha . 90)))
+  ((mac ns) (add-to-list 'default-frame-alist '(alpha . 95)))
+  ((w32)    (add-to-list 'default-frame-alist '(alpha . 90)))
   )
 
-;---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
 ;;; 対応括弧をHighlight表示
 (show-paren-mode t)
 ;;; tと追加可能
@@ -50,51 +28,61 @@
 ;; (set-face-background 'show-paren-match-face "plum2")
 ;; (set-face-foreground 'show-paren-match-face "Blue")
 
-;---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
 ;;; 行末Space, Tabの表示
 (setq-default show-trailing-whitespace t)
 (set-face-background 'trailing-whitespace "Pink")
 
-;---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
 ;;; 選択regionの色
 (setq-default transient-mark-mode t)
-(if window-system
-    (set-face-background 'region "DarkSlateBlue")
-  (set-face-background 'region "LightBlue")
-  )
+;; (if window-system
+;;     (set-face-background 'region "DarkSlateBlue")
+;;   (set-face-background 'region "LightBlue")
+;;   )
 
-;---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
 ;;; IMEのON/OFFでCursorの色を変える
 ;; XXX: Buffer移動のときに誤表示?
 (set-cursor-color "#fd3")
-(cond
+(case window-system
 
- ((eq window-system 'x)
-  (add-hook 'input-method-activate-hook
-            (function (lambda () (set-cursor-color "Magenta"))))
-  (add-hook 'input-method-inactivate-hook
-            (function (lambda () (set-cursor-color "#fd3"))))
+  (x
+   (add-hook 'input-method-activate-hook
+             (lambda () (set-cursor-color "Magenta")))
+   (add-hook 'input-method-inactivate-hook
+             (lambda () (set-cursor-color "#fd3")))
+   )
+
+  (w32
+   (add-hook 'w32-ime-on-hook
+             (lambda () (set-cursor-color "Magenta")))
+   (add-hook 'w32-ime-off-hook
+             (lambda () (set-cursor-color "#fd3")))
+   )
+
+  (mac
+
+   (add-hook 'mac-selected-keyboard-input-source-change-hook
+             (lambda ()
+               (set-cursor-color
+                (if (string-match "\\.\\(US\\|Roman\\)$" (mac-input-source))
+                    "#fd3" "SkyBlue"))
+               )
+             )
+   )
   )
 
- ((eq window-system 'w32)
-  (add-hook 'w32-ime-on-hook
-            (function (lambda () (set-cursor-color "Magenta"))))
-  (add-hook 'w32-ime-off-hook
-            (function (lambda () (set-cursor-color "#fd3"))))
-  )
-
- )
-
-;---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
 ;;; 現在行をHighlight
 ;;; 色指定
 (defface my-hlline-face
   (if window-system
 
       '((((class color) (background dark))
-         (:background "gray20"))
+         (:background "gray30"))
         (((class color) (background light))
-         (:background "gray20"))
+         (:background "gray30"))
         (t ()))
 
     '((((class color) (background dark))
@@ -112,8 +100,7 @@
 ;;; 有効
 (global-hl-line-mode)
 
-
-;---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
 ;;; 不可視文字の表示 (jaspace-modeでも)
 ;;; 強調表示するもの
 ;; (setq whitespace-style
@@ -156,26 +143,27 @@
     )
   )
 
-;---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
 ;;; Buffer中のKeywordを色付け
 (global-font-lock-mode t)
 ;;; 色付け方
 ;; (setq font-lock-support-mode 'jit-lock-mode)
 
-;---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
 ;;; 正規表現のGroupingを色付け
-(if window-system
-    (progn
+;; (if window-system
+    ;; (progn
       ;; (set-face-foreground 'font-lock-regexp-grouping-backslash "orange")
       ;; (set-face-background 'font-lock-regexp-grouping-backslash "white")
       ;; (set-face-foreground 'font-lock-regexp-grouping-construct "red")
       ;; (set-face-background 'font-lock-regexp-grouping-construct "green")
-      )
+      ;; )
 
-    (progn
+    ;; (progn
       ;; (set-face-foreground 'font-lock-regexp-grouping-backslash "orange")
       ;; (set-face-background 'font-lock-regexp-grouping-backslash "white")
       ;; (set-face-foreground 'font-lock-regexp-grouping-construct "red")
       ;; (set-face-background 'font-lock-regexp-grouping-construct "green")
-      )
-    )
+      ;; )
+    ;; )
+
