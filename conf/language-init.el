1bc58227fec93817c79b3f89ecd9e560497e156b
skkzsh <skkzsh12@gmail.com>
2019-07-06 19:59:28 +0900

    Update lang config

--- conf/language-init.el
+++ conf/language-init.el
@@ -1,5 +1,5 @@
 ;;;; Programming Languages Settings
-;---------------------------------------------------------------------------
+;;---------------------------------------------------------------------------
 ;;;; Compile
 ;; TODO
 (setq compilation-window-height 12) ;; デフォルトは画面の下半分
@@ -16,58 +16,91 @@
 ; (global-set-key "\C-c c" 'smart-compile)
 
 
-;---------------------------------------------------------------------------
+;; flycheck
+;; (global-flycheck-mode)
+;; (add-hook 'after-init-hook #'global-flycheck-mode)
+(dolist (hook '(
+                ;; yaml-mode-hook
+                xml-mode-hook
+                html-mode-hook
+                css-mode-hook
+                js-mode-hook
+                ;; markdown-mode-hook
+                ;; sh-mode-hook
+                sql-mode-hook
+                ))
+  (add-hook hook 'flycheck-mode)
+  )
+
+;;---------------------------------------------------------------------------
 ;;;; Ruby, Perl, Python
 ;; (when (require 'rcodetools nil t)
 ;;   (add-hook 'ruby-mode-hook
-;;             (lambda()
+;;             (lambda ()
 ;;               (define-key ruby-mode-map (kbd "C-c e") 'xmp)
 ;;               )))
 
+;;---------------------------------------------------------------------------
 ;;;; Lisp
 (require 'lispxmp)
 (add-hook 'emacs-lisp-mode-hook
-          (lambda()
+          (lambda ()
             (define-key emacs-lisp-mode-map (kbd "C-c e") 'lispxmp)
             ))
-;; (add-hook 'common-lisp-mode-hook
-;;           (lambda()
-;;             (define-key common-lisp-mode-map (kbd "C-c e") 'lispxmp)
+(add-hook 'common-lisp-mode-hook
+          (lambda ()
+            (define-key common-lisp-mode-map (kbd "C-c e") 'lispxmp)
+            ))
+;; (add-hook 'slime-mode-hook
+;;           (lambda ()
+;;             (define-key slime-mode-map (kbd "C-c e") 'lispxmp)
 ;;             ))
-;; (add-hook 'scheme-mode-hook
-;;           (lambda()
-;;             (define-key scheme-mode-map (kbd "C-c e") 'lispxmp)
-;;             ))
-;; (add-hook 'lisp-mode-hook
-;;           (lambda()
-;;             (define-key lisp-mode-map (kbd "C-c e") 'lispxmp)
-;;             ))
+(add-hook 'lisp-mode-hook
+          (lambda ()
+            (define-key lisp-mode-map (kbd "C-c e") 'lispxmp)
+            ))
 
+;; Common Lisp
+;; (add-to-list 'auto-mode-alist '("\\.cl$" . common-lisp-mode))
+
+(if (file-readable-p "~/.roswell/helper.el")
+    (load (expand-file-name "~/.roswell/helper.el"))
+  )
+
+;;---------------------------------------------------------------------------
 ;;;; Zsh
 (add-to-list 'auto-mode-alist '("\\.zsh$" . sh-mode))
 ;; (add-to-list 'auto-mode-alist '("\\.zsh$" . shell-script-mode))
 
+;;;; PlantUML
+(when (executable-find "plantuml")
+  (add-to-list 'auto-mode-alist '("\\.pu$" . plantuml-mode))
+  )
+;; (setq plantuml-output-type "utxt") ; 有効にならない
+
+;;---------------------------------------------------------------------------
 ;;;; PowerShell
 ;; TODO hook encoding
-(autoload 'powershell-mode "powershell-mode" "Mode PowerShell" t)
-(push '("\\.\\(ps1\\|psd1\\|psm1\\)$" . powershell-mode) auto-mode-alist)
-;; (push '("\\.ps[12]?$" . powershell-mode) auto-mode-alist)
+;; (autoload 'powershell-mode "powershell-mode" "Mode PowerShell" t)
+;; (add-to-list 'auto-mode-alist '("\\.\\(ps1\\|psd1\\|psm1\\)$" . powershell-mode))
 
 ;;;; Batch
 ;; TODO hook encoding
-(require 'batch-mode)
+;; (require 'batch-mode)
 
 ;;;; Visual Basic
-(autoload 'visual-basic-mode "visual-basic-mode" "Visual Basic mode." t)
-(setq auto-mode-alist
-    (append '(
-              ("\\.vb$"  . visual-basic-mode)
-              ("\\.vbs$" . visual-basic-mode)
-              ("\\.bas$" . visual-basic-mode)
-              ("\\.frm$" . visual-basic-mode)
-              ;; ("\\.cls$" . visual-basic-mode)
-              ) auto-mode-alist))
+;; (autoload 'visual-basic-mode "visual-basic-mode" "Visual Basic mode." t)
+;; (setq auto-mode-alist
+;;     (append '(
+;;               ("\\.vb$"  . visual-basic-mode)
+;;               ("\\.vbs$" . visual-basic-mode)
+;;               ("\\.bas$" . visual-basic-mode)
+;;               ("\\.frm$" . visual-basic-mode)
+;;               ;; ("\\.cls$" . visual-basic-mode)
+;;               )
+;;               auto-mode-alist))
 
+;;---------------------------------------------------------------------------
 ;;;; Gnuplot
 ;; (add-to-list 'auto-mode-alist
 ;;     '("\\.\\(gp\\|plt\\)$" . gnuplot-mode))
